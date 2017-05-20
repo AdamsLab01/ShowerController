7893a72d517c6cf8a82b3e11711bbcdfcdc192d8
rocketboy001 <adam@adambyers.com>
2017-05-20 08:54:00 -0700

    bla

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,4 +1,4 @@
-/*
+ /*
 This code was written to control a solenoid valve in order to manage the amount of time our show could be used.
 
 More info can be found here: https://youtu.be/1Lek2_mfyv4 and here: https://adambyers.com/2017/05/shower-controller-manage-shower-length/
